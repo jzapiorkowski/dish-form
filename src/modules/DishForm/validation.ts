4b5c6f17314bcd9e83ee0158b839181bfeaad215
Jakub Zapiórkowski <jakub.zapiorkowski@palturai.com>
2023-05-06 18:30:49 +0200

    enhance validation

--- src/modules/DishForm/validation.ts
+++ src/modules/DishForm/validation.ts
@@ -4,6 +4,9 @@ import { DishFormFieldNames, DishType } from './types';
 import { DishFormLabels } from './constants';
 
 const requiredError = (fieldName: DishFormFieldNames) => `${DishFormLabels[fieldName]} is required`;
+const numberTypeError = (fieldName: DishFormFieldNames) => `${DishFormLabels[fieldName]} must be a number`;
+const minValueError = (fieldName: DishFormFieldNames, value: number) =>
+  `${DishFormLabels[fieldName]} must be at least ${value}`;
 
 export const validationSchema = yupResolver(
   yup.object({
@@ -13,9 +16,25 @@ export const validationSchema = yupResolver(
       .mixed<DishType>()
       .oneOf(Object.values(DishType))
       .required(({ path }) => requiredError(path)),
-    [DishFormFieldNames.NO_OF_SLICES]: yup.number().required(({ path }) => requiredError(path)),
-    [DishFormFieldNames.DIAMETER]: yup.number().required(({ path }) => requiredError(path)),
-    [DishFormFieldNames.SPICINESS_SCALE]: yup.number().required(({ path }) => requiredError(path)),
-    [DishFormFieldNames.SLICES_OF_BREAD]: yup.number().required(({ path }) => requiredError(path)),
+    [DishFormFieldNames.NO_OF_SLICES]: yup
+      .number()
+      .typeError(({ path }) => numberTypeError(path))
+      .min(1, ({ path }) => minValueError(path as DishFormFieldNames, 1))
+      .required(({ path }) => requiredError(path)),
+    [DishFormFieldNames.DIAMETER]: yup
+      .number()
+      .typeError(({ path }) => numberTypeError(path))
+      .min(1, ({ path }) => minValueError(path as DishFormFieldNames, 1))
+      .required(({ path }) => requiredError(path)),
+    [DishFormFieldNames.SPICINESS_SCALE]: yup
+      .number()
+      .typeError(({ path }) => numberTypeError(path))
+      .min(1, ({ path }) => minValueError(path as DishFormFieldNames, 1))
+      .required(({ path }) => requiredError(path)),
+    [DishFormFieldNames.SLICES_OF_BREAD]: yup
+      .number()
+      .typeError(({ path }) => numberTypeError(path))
+      .min(1, ({ path }) => minValueError(path as DishFormFieldNames, 1))
+      .required(({ path }) => requiredError(path)),
   }),
 );
